924af5d527c94f6df7556a4c50254908f7071af4
Liezl Mouton <liz.mouton1@gmail.com>
2024-12-19 21:08:23 +0200

    Minor change

--- Week 1/w1_day_2/src/app/product/page.tsx
+++ Week 1/w1_day_2/src/app/product/page.tsx
@@ -6,13 +6,13 @@ const Products: React.FC = () => {
             <h1 className="text-4xl font-bold mt-10 text-center">Products</h1>
             <div className="flex flex-row justify-center mt-10">
                 <li className="mr-5">
-                    <Link href="" className="hover:underline">Product 1</Link>
+                    <Link href="/product/1" className="hover:underline">Product 1</Link>
                 </li>
                 <li className="mr-5">
-                    <Link href="" className="hover:underline">Product 2</Link>
+                    <Link href="/product/2" className="hover:underline">Product 2</Link>
                 </li>
                 <li className="mr-5">
-                    <Link href="" className="hover:underline">Product 3</Link>
+                    <Link href="/product/3" className="hover:underline">Product 3</Link>
                 </li>
             </div>
 
