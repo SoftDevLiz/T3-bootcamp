0988a7e22c9f2768c3d1fe2b1a2beccbd4ec9c93
Liezl Mouton <liz.mouton1@gmail.com>
2024-12-30 14:42:58 +0200

    Refactor to async / await for dynamic parameter fetching

--- Week 1/w1_day_2/src/app/product/[sku]/page.tsx
+++ Week 1/w1_day_2/src/app/product/[sku]/page.tsx
@@ -2,12 +2,12 @@ interface ProductPageProps {
     params: { sku: string };
 }
 
-const ProductPage: React.FC<ProductPageProps> = ({ params }) => {
-    const { sku } = params;
+async function ProductPage({ params }: ProductPageProps): Promise<JSX.Element> {
+    const { sku } = await params;
 
     return (
         <div className="flex items-center justify-center h-screen font-mono text-4xl">
-            <h2>You are viewing SKU {sku}</h2>
+            <h2>You are viewing SKU { sku }</h2>
         </div>
     )
 }
