be5561530277b499a020ce5ab40dfb636a8d398d
Liezl Mouton <liz.mouton1@gmail.com>
2025-01-20 16:00:39 +0200

    Setup home page

--- Week 1/w1_day_5/tailwind.config.ts
+++ Week 1/w1_day_5/tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
+      backgroundImage: {
+        bodyGradient: "linear-gradient(180deg,rgb(15, 2, 46) 0%,rgb(22, 2, 82) 100%, rgb(32, 3, 100) 0%, rgb(43, 3, 135) 0%, rgb(57, 7, 173) 0%)",
+      },
     },
   },
   plugins: [],
