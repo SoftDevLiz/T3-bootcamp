e29cfc82a4bc771eb04ddafeed7d3960f47e842a
Liezl Mouton <liz.mouton1@gmail.com>
2025-01-20 19:00:31 +0200

    Set up form

--- Week 1/w1_day_5/src/app/page.tsx
+++ Week 1/w1_day_5/src/app/page.tsx
@@ -1,7 +1,52 @@
-export default async function Home() {
+"use client"
+import React, { useState } from 'react';
+
+export default function Home() {
+  const [formData, setFormData] = useState( {email: "Email address", message: "Type your message here"} )
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData, [name]: value,
+    }))
+  }
+
   return (
     <div>
-      <h1 className='flex justify-center text-white p-5'>A T3 Stack Form</h1>
+      <header className='flex justify-center'>
+        <h1 className='text-white p-12 text-4xl'>A T3 Stack Form ✨</h1>
+      </header>
+      <main>
+      <form className='flex flex-col items-center space-y-5'>
+        <input
+          id='email'
+          type='email'
+          name='email'
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <input
+          id='message'
+          type='text'
+          name='message'
+          value={formData.message}
+          onChange={handleChange}
+        />
+        <button>Submit</button>
+      </form>
+      </main>
     </div>
   );
 }
+
+/*
+TODO: 
+- A simple contact form
+
+- Email address (text input)
+- Message (text input)
+- Submit button (submit)
+- "We'll be in touch" response
+
+- Emails message to my gmail
+*/
