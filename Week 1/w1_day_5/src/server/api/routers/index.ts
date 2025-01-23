9c3257758a80467be0874006f000c05ec8dbb6c1
Liezl Mouton <liz.mouton1@gmail.com>
2025-01-23 15:34:50 +0200

    Remove TRPC router

--- Week 1/w1_day_5/src/server/api/routers/index.ts
+++
@@ -1,8 +0,0 @@
-import { createTRPCRouter } from '../trpc';
-import { contactFormRouter } from './contactForm';
-import { mergeRouters } from '@trpc/server/unstable-core-do-not-import';
-
-export const appRouter = mergeRouters(
-  createTRPCRouter({}),
-  contactFormRouter
-);
